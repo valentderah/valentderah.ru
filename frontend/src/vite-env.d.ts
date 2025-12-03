1b61ee920e37206f35b46689a9cca761cf3d9d51
Valiantsin Dzerakh <valentderah@gmail.com>
2025-12-03 16:53:20 +0300

    fix config

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+declare module "tempo-routes" {
+  import { RouteObject } from "react-router-dom";
+  const routes: RouteObject[];
+  export default routes;
+}
